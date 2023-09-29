8843c423913a22fa6b1b1b5923525c0aa570ed6b
markanfit <muharkan04@gmail.com>
2023-09-29 12:45:57 +0700

    Tambah Identitas Mahasiswa

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01{
+    public static void main(String[] args) {
+       System.out.print("Name" + ":");
+       System.out.println("Muhammad Arkan"); 
+       System.out.print("NIM" + ":");
+       System.out.println("607062330099");
+       System.out.print("Class" + ":");
+       System.out.println("D3IF 47 01");
+    }
+
+}
