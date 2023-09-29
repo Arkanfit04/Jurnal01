f5f3596921476c1748c88887ce57e5adf9ec905b
markanfit <muharkan04@gmail.com>
2023-09-29 19:52:05 +0700

    fix commit identitas mahasiswa

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,11 +1,9 @@
 public class Jurnal01{
     public static void main(String[] args) {
-       System.out.print("Name" + ":");
-       System.out.println("Muhammad Arkan"); 
-       System.out.print("NIM" + ":");
-       System.out.println("607062330099");
-       System.out.print("Class" + ":");
-       System.out.println("D3IF 47 01");
+      System.out.println("Name : Muhammad Arkan");
+      System.out.println("NIM : 607062330099");
+      System.out.println("Kelas : D3 RPLA 47 01");
+
     }
 
 }
